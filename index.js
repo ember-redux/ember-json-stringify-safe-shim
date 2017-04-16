4c2155dce4057914fbc4be8cf41cd65670efaad9
toran billups <toranb@gmail.com>
2017-04-15 20:55:45 -0500

    init

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 /* eslint-env node */
 'use strict';
 
+const mergeTrees = require('broccoli-merge-trees');
+const path = require('path');
+
 module.exports = {
-  name: 'ember-json-stringify-safe-shim'
-};
+  name: 'json-stringify-safe',
+  treeForAddon (tree) {
+    const stringifyPath = path.dirname(require.resolve('json-stringify-safe/stringify.js'));
+    let stringifyTree = this.treeGenerator(stringifyPath);
+
+    if (!tree) {
+      return this._super.treeForAddon.call(this, stringifyTree);
+    }
+
+    const trees = mergeTrees([stringifyTree, tree], {
+      overwrite: true
+    });
+
+    return this._super.treeForAddon.call(this, trees);
+  }
+}
